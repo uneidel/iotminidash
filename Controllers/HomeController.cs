416a006a284228e6f7713e80a113ba1fafb4005c
uneidel <uneidel@octonion.de>
2018-03-20 18:57:29 +0100

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using IoTHubTempWebApp.Hubs;
+using Microsoft.AspNet.SignalR;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IoTHubTempWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string connectionString = "Endpoint=sb://miotybus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=VSPngRu2xmUc8efY37MvMxPU1nldMGBMohNgJam7j04=";
+        private string queueName = "mioty";
+        private IHubContext _hubContext;
+
+        public HomeController()
+        {
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<IoTHub>();
+        }
+
+        public ActionResult Index()
+        {
+            Task task = Task.Run(() =>
+            {
+
+                QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName, ReceiveMode.ReceiveAndDelete);
+
+                client.OnMessage(message =>
+                {
+                    Stream stream = message.GetBody<Stream>();
+                    StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                    string s = reader.ReadToEnd();
+                    _hubContext.Clients.All.ioTHubNotification(s);
+
+                });
+            });
+
+            task.Wait();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
